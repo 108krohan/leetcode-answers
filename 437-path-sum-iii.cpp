d26bac101e9e58896c9413b748aaff6159629d81
Rohan Kumar <108krohan@users.noreply.github.com>
2020-04-03 15:00:35 +0530

    Easy
    https://leetcode.com/problems/path-sum-iii/

---
+++ 437-path-sum-iii.cpp
@@ -0,0 +1,57 @@
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+Example:
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int countSum(TreeNode* root, int targetSum) {
+        if(!root) return 0;
+        int result = 0;
+        if(root->val == targetSum) {
+            result += 1; 
+        }
+        return result + countSum(root->left, targetSum - root->val)
+            + countSum(root->right, targetSum - root->val)
+            + countSum(root->left, targetSum) 
+            + countSum(root->right, targetSum);
+        
+
+    }
+    int pathSum(TreeNode* root, int sum) {
+        if(!root) return 0;
+        return countSum(root, sum);
+    }
+};
